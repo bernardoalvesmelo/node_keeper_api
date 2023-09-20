38ae317164f80d8a27a9f0245119a4a506f66e94
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-09-20 15:38:03 -0300

    corrigind server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use('/api', router)
 
-const porta = process.evn.PORT | 3000;
+const porta = process.env.PORT | 3000;
 
 server.listen(3000, () => {
   console.log(`A API do NoteKeeper está rodando na porta ${porta}`)
